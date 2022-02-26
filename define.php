be8678e41601034b3401a255b3cf5e5f7b656d25
tsuka-hiro <hiroyuki.10191019@gmail.com>
2022-02-27 00:25:34 +0900

    first commit

--- define.php
+++ define.php
@@ -1,4 +1,4 @@
 <?php
-define('ACCESS_TOKEN','');  //アクセストークン
-define('CHANNEL_SECRET','');  //シークレット
+define('ACCESS_TOKEN','5X06O+1L1PKV4O0iU7e7ZbdsxSKZJ6QjNRDXy1GwO73J59cONaNy0dANchDiEcAmbwp9wGq1qY04fWA15lgrnhbHEAdTuT5F7Yeezr5zaN/i8WJpopteusAJP5WA4aAGf2dFr/IroClbe3uAPiN0mAdB04t89/1O/w1cDnyilFU=');  //アクセストークン
+define('CHANNEL_SECRET','d70ad7bd895d21c84f380b36bab415b8');  //シークレット
 ?>
